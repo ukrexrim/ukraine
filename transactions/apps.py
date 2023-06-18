6ab00aa93a23eedb9bcc48ad61cd222fbdea2c4c
tonydasr <136280620+tonydasr@users.noreply.github.com>
2023-06-18 01:18:09 +0100

    Delete transactions directory

--- transactions/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-from django.apps import AppConfig
-
-class TransactionsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'transactions'
-    verbose_name = 'Manage Transactions'
